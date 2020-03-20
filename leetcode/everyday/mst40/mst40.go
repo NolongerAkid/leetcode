87ceb7410b7d4bf10838f2fec4a72f91f9d1fcbe
NoLongerAkid <502040653@qq.com>
2020-03-20 17:30:49 +0800

    add mst40

--- leetcode/everyday/mst40/mst40.go
+++ leetcode/everyday/mst40/mst40.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+func getLeastNumbers(arr []int, k int) []int {
+	sort.Ints(arr)
+	return arr[0:k]
+
+}
+
 func main() {
+
+	fmt.Println(getLeastNumbers([]int{1, 2, 3}, 2))
 
 }
